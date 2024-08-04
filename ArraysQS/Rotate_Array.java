7a460321a8718fdaac9ec170b3e2a75ba1eef3bc
khushi09s <khushi.s210929@gmail.com>
2024-08-04 13:09:19 +0530

    DONE

--- ArraysQS/Rotate_Array.java
+++ ArraysQS/Rotate_Array.java
@@ -3,7 +3,7 @@ package ArraysQS;
 public class Rotate_Array {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-        int k = 4;
+        int k = 5;
         Rotate(arr, k);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
